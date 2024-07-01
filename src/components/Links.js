db9d70a145405990f3d2a1f088e47f3daa755795
atsilcock <143049271+atsilcock@users.noreply.github.com>
2024-07-01 15:21:59 -0600

    basics with props

---
+++ src/components/Links.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Links ({github, linkedin}) {
+  
+  
+
+  return (
+   <div>
+
+   <h3> Links
+     <a href= {github}>https://github.com/liza</a>
+      <a href={linkedin}>https://www.linkedin.com/in/liza/</a>
+   </h3>
+
+   </div>
+  );
+}
+
+export default Links;
